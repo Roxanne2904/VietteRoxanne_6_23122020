4a726a52f8c05742580d30631c96f4ee49f212c7
Roxanne <roxanne-viette@hotmail.fr>
2021-06-03 14:12:04 +0200

    Mise en place du header et du main sur la page index

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 //Ouvrir la modal//Opening Modal
-const isOpeningTheModal =()=>{
- const bgdModal = document.querySelector(".modal");
- bgdModal.style.display="block";
-}
+const isOpeningTheModal = () => {
+  const bgdModal = document.querySelector(".modal");
+  bgdModal.style.display = "block";
+};
 
-const isLaunchingTheOpenModalEvent =()=>{
-    const openModal = document.querySelector("#openModal");
-    openModal.addEventListener('click',(isOpeningTheModal))
-}
+const isLaunchingTheOpenModalEvent = () => {
+  const openModal = document.querySelector("#openModal");
+  openModal.addEventListener("click", isOpeningTheModal);
+};
 isLaunchingTheOpenModalEvent();
 //______________________________
 //fermer la modal//Closing Modal
-const isClosingTheModal = ()=>{
- const bgdModal = document.querySelector(".modal");
- bgdModal.style.display="none";
-}
-const isLaunchingTheCloseModalEvent=()=>{
-    const closeModal = document.querySelector(".iconeX");
-    closeModal.addEventListener('click',(isClosingTheModal))
-}
+const isClosingTheModal = () => {
+  const bgdModal = document.querySelector(".modal");
+  bgdModal.style.display = "none";
+};
+const isLaunchingTheCloseModalEvent = () => {
+  const closeModal = document.querySelector(".iconeX");
+  closeModal.addEventListener("click", isClosingTheModal);
+};
 isLaunchingTheCloseModalEvent();
